d1bb49584b07f7902c52cf5c78275564a414fb2c
therandomedud <inas.alkhafaf@gmail.com>
2023-06-02 11:16:52 +0200

    ready for realease

--- src/main/java/de/therandomdud/Main.java
+++ src/main/java/de/therandomdud/Main.java
@@ -12,11 +12,5 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
        Terminal.terminal();
     }
-/*
- EmailScraper scraper = new EmailScraper("placeholder","placeholder","placeholder");
-        scraper.login();
-        ArrayList<Email> emailsReceive = scraper.scrapeSendMails();
-        System.out.println(emailsReceive);
-        JSONUtils.saveEmails(emailsReceive);
- */
+
 }
